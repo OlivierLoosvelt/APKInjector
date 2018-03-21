fa7407f95f1fdea50e8a7f60c9c2aad88b30c626
root <olivier.loosvelt@hotmail.com>
2018-03-21 13:55:57 +0100

    Revert "APKTool integration added"
    This reverts commit d3139bfbfadf22dddf43fe8c804c4352f8f3ef1b.

--- MainFunctionCatcher.py
+++ MainFunctionCatcher.py
@@ -2,28 +2,16 @@
 from bs4 import BeautifulSoup as Soup
 from time import sleep
 import sys
-import os
-import subprocess
 
-soup = ""
+
+file = open(sys.argv[1])
+page = file.read()
+soup = Soup(page, "xml")
 
 def findPackage():
-    #os.system('apktool --version')
-    global soup
-    command = ["pwd"]
-    cmd = subprocess.Popen(command, stdout=subprocess.PIPE)
-    curr_dir = cmd.communicate()[0].replace('\r', '').replace('\n', '')
-
-
-    appname = sys.argv[1].split('.')[0]
-    manifest_file = curr_dir + '/' + appname + '/AndroidManifest.xml'
-    file = open(manifest_file)
-    page = file.read()
-    soup = Soup(page, "xml")
     manifest = soup.find_all('activity')
     main_activity = manifest[0]['android:name']
-    checkMain(main_activity) 
-
+    checkMain(main_activity)
 
 def checkMain(activity):
     str_len = len(activity.split('.'))
@@ -33,22 +21,11 @@ def checkMain(activity):
         print('Main Activity: ' + changeToFolder(activity))
 
 def addPackage(name):
-    global soup
     manifest = soup.find_all('manifest')
-    print('\r\nMain Activity: '+ changeToFolder(manifest[0]['package'] + name))
+    print('Predicted main: '+ changeToFolder(manifest[0]['package'] + name))
 
 def changeToFolder(string):
     return string.replace('.', '/')
-
-def init():
-    command = ["apktool", "d", "-f", sys.argv[1]]
-    cmd = subprocess.Popen(command, stdout=subprocess.PIPE)
-    while True:
-        line = cmd.stdout.readline().rstrip()
-        if not line:
-            break
-        print line
-    findPackage()
 
 if __name__ == "__main__":
     sleep(0.1)
@@ -60,4 +37,4 @@ if __name__ == "__main__":
     print(" |_|  |_|\__,_|_| |_|_|_|  \___||___/\__| /_/   \_\_| |_|\__,_|_|\__, /___\___|_|   ")
     print("                                                                 |___/              ")
     print("                                                                                    ")
-    init()
+    findPackage()
